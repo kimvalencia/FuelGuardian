0c8f5d90b666e833d786255b245cbaf478af7530
Kim Valencia <kimvalencia@gmail.com>
2025-08-21 00:07:23 +0800

    Added controller for Vehicle CRUDS

---
+++ FuelGuardianWebService/FuelGuardianWebService.App/DTOs/VehicleAgg/AddVehicleDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelGuardianWebService.App.DTOs.VehicleAgg
+{
+    public class AddVehicleDTO
+    {
+        public string VehicleType { get; set; } = string.Empty;
+        public string PlateNumber { get; set; } = string.Empty;
+    }
+}
---
+++ FuelGuardianWebService/FuelGuardianWebService/Controllers/VehicleController.cs
@@ -0,0 +1,51 @@
+﻿using FuelGuardianWebService.App;
+using FuelGuardianWebService.App.DTOs.VehicleAgg;
+using FuelGuardianWebService.App.Entities;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FuelGuardianWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly FuelGuardianDBContext dbContext;
+        private readonly ILogger<VehicleController> logger;
+        public VehicleController(FuelGuardianDBContext context, ILogger<VehicleController> logger)
+        {
+            dbContext = context;
+            this.logger = logger;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var vehicles = dbContext.Vehicles.ToList() ;
+
+            return Ok(vehicles); 
+        }
+
+        [HttpGet,Route("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var vehicle = dbContext.Vehicles.SingleOrDefault(x => x.Id == id);
+
+            if (vehicle == null)
+                return NoContent();
+            return Ok(vehicle);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AddVehicleDTO addVehicle)
+        {
+            Vehicle vehicle = addVehicle.Adapt<Vehicle>();
+            dbContext.Vehicles.Add(vehicle);
+            dbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = vehicle.Id }, vehicle);
+        }
+    }
+}
