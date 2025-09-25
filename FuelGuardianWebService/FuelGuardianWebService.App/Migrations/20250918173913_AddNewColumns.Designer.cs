3fe710b906906d5a43b3b46766644fb5e5bed8dd
Kim Valencia <kimvalencia@gmail.com>
2025-09-26 00:46:57 +0800

    add migration to new columns

--- FuelGuardianWebService/FuelGuardianWebService.App/Migrations/20250918173913_AddNewColumns.Designer.cs
+++ FuelGuardianWebService/FuelGuardianWebService.App/Migrations/20250918173913_AddNewColumns.Designer.cs
@@ -4,6 +4,7 @@ using FuelGuardianWebService.App;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FuelGuardianWebService.App.Migrations
 {
     [DbContext(typeof(FuelGuardianDBContext))]
-    partial class FuelGuardianDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250918173913_AddNewColumns")]
+    partial class AddNewColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FuelGuardianWebService/FuelGuardianWebService.App/Migrations/20250918173913_AddNewColumns.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FuelGuardianWebService.App.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "FuelQuantityUsed",
+                table: "FuelUsages",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaid",
+                table: "BillingHeaders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Remarks",
+                table: "BillingHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FuelQuantityUsed",
+                table: "FuelUsages");
+
+            migrationBuilder.DropColumn(
+                name: "IsPaid",
+                table: "BillingHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "Remarks",
+                table: "BillingHeaders");
+        }
+    }
+}
